98c19ece37f9faa7c424063554b820e694f8f70d
Hariprakash3102 <hariprakash.srinivasan@concertidc.com>
2024-10-16 11:44:05 +0530

    correction in CreateLead

--- Field_Groove.Web/Controllers/HomeController.cs
+++ Field_Groove.Web/Controllers/HomeController.cs
@@ -41,6 +41,7 @@ namespace Field_Groove.Web.Controllers
         {
             return View();
         }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
