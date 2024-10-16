10c31d6b3f9ab22a08e5215e1cd75aef9c49296d
NithishCIDC <nithish.sakthivelmurugan@concertidc.com>
2024-10-16 11:28:39 +0530

    Added CreateLead Controller

--- Field_Groove.Web/Controllers/HomeController.cs
+++ Field_Groove.Web/Controllers/HomeController.cs
@@ -6,42 +6,45 @@ using System.Diagnostics;
 
 namespace Field_Groove.Web.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
-		private readonly IConfiguration _configuration;	
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-		public HomeController(ILogger<HomeController> logger , IConfiguration configuration)
-		{
-			_logger = logger;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
             _configuration = configuration;
         }
 
-		public IActionResult Index()
-		{
-			return RedirectToAction("Login","Account");
-		}
+        public IActionResult Index()
+        {
+            return RedirectToAction("Login", "Account");
+        }
 
         [HttpGet]
         public IActionResult Dashboard()
         {
-			ViewBag.Username = _configuration["UserDetails:Email"];
+            ViewBag.Username = _configuration["UserDetails:Email"];
             return View();
         }
-		
-		[HttpGet]
+
+        [HttpGet]
         public IActionResult Leads()
         {
             ViewBag.Username = _configuration["UserDetails:Email"];
             return View();
         }
 
-
-
+        [HttpGet]
+        public IActionResult CreateLead()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
