002eb6f07a044b0a5e81399fc43700d0371144eb
Hariprakash3102 <hariprakash.srinivasan@concertidc.com>
2024-10-16 12:44:33 +0530

    Leads Model

--- Field_Groove.Infrastructure/Migrations/20241016065352_Add Leads Table.Designer.cs
+++ Field_Groove.Infrastructure/Migrations/20241016065352_Add Leads Table.Designer.cs
@@ -4,6 +4,7 @@ using Field_Groove.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Field_Groove.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241016065352_Add Leads Table")]
+    partial class AddLeadsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
