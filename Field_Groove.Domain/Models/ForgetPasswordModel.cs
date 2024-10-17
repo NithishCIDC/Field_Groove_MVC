fc3fa9133d204974ce09a541fdb3254015923c3d
Hariprakash3102 <hariprakash.srinivasan@concertidc.com>
2024-10-17 17:15:28 +0530

    Added  forgot Password

---
+++ Field_Groove.Domain/Models/ForgetPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Field_Groove.Domain.Models
+{
+    public class ForgetPasswordModel
+    {
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
